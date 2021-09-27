8ddbfba43e326085df35b8becc8be0e46b59c3a0
LukasPinto <lukas.pinto@alumnos.uv.cl>
2021-09-26 22:09:28 -0300

    Proyecto veterinaria

---
+++ Control_veterinaria/src/database.js
@@ -0,0 +1,19 @@
+const mysql=require('mysql');
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'prueba',
+})
+
+conn.connect(function(err){
+    if(err){
+        console.log(err);
+        return;
+    }else{
+        console.log('La base de datos se conecto')
+    }
+});
+
+module.exports=conn;
---
+++ Control_veterinaria/src/index.js
@@ -0,0 +1,18 @@
+const bodyParser=require ('body-parser');
+const express = require ('express');
+const app = express();
+const path = require('path');
+
+//configuracion
+app.set('port',process.env.PORT || 3000);
+
+//middleware
+app.use(express.json());
+
+//rutas
+//app.use(require('./routes/consultas'));
+
+//servidor
+app.listen(app.get('port'), ()=> {
+    console.log('servidor en Puerto',app.get('port'))
+});
